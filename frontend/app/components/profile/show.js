3a8bc5104ad7dcb2e35257d78c17221bfc310a42
Don Omondi <don.e.omondi@gmail.com>
2020-01-26 21:34:47 +0300

    Updates

--- frontend/app/components/profile/show.js
+++ frontend/app/components/profile/show.js
@@ -1,4 +1,8 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class ProfileShowComponent extends Component {
+
+  @service session;
+
 }
