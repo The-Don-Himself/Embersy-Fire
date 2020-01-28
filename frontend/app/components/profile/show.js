8a440cba8d5ebd0e5345cbeb097b046c0aaffc4c
Don Omondi <don.e.omondi@gmail.com>
2020-01-28 19:17:31 +0300

    Updates

--- frontend/app/components/profile/show.js
+++ frontend/app/components/profile/show.js
@@ -1,8 +1,18 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ProfileShowComponent extends Component {
 
   @service session;
+
+  @action
+  setMDCTextField(element) {
+    let MDCTextField = mdc.textField.MDCTextField;
+
+    setTimeout(function(){
+      new MDCTextField(element);
+    },750);
+  }
 
 }
